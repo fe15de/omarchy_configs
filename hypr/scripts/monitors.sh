9c69747c5bb0f21a24b59f8e6f0a705a54981222
Federico <federicoperezramirez@gmail.com>
2026-05-29 22:02:36 -0500

    Fix monitor script

--- hypr/scripts/monitors.sh
+++ hypr/scripts/monitors.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
 handle_monitors() {
-    if hyprctl monitors | grep -q "HDMI-A-1"; then
-        # HDMI connected
-        hyprctl keyword monitor "HDMI-A-1,preferred,auto,1"
-        hyprctl keyword monitor "eDP-1,disable"
+    if hyprctl monitors all | grep -q "HDMI-A-1"; then
+        if hyprctl monitors | grep -q "eDP-1"; then
+            # Laptop activa → cambiar a solo monitor externo
+            hyprctl keyword monitor "HDMI-A-1,preferred,auto,1"
+            hyprctl keyword monitor "eDP-1,disable"
+        else
+            # Monitor externo activo → cambiar a solo laptop
+            hyprctl keyword monitor "eDP-1,preferred,auto,1"
+            hyprctl keyword monitor "HDMI-A-1,disable"
+        fi
     else
-         
+        # HDMI desconectado físicamente → forzar laptop
         hyprctl keyword monitor "eDP-1,preferred,auto,1"
     fi
 }
-
 
 handle_monitors
